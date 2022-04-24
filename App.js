8cbac0cb06491a38544d9a439539925ab6920c84
Finn <fluxr3@gmail.com>
2022-04-25 10:31:58 +1200

    render results list

--- App.js
+++ App.js
@@ -1,9 +1,11 @@
 import { createAppContainer } from "react-navigation";
 import { createStackNavigator } from "react-navigation-stack";
 import SearchScreen from "./src/screens/SearchScreen";
+import ResultsList from "./src/components/ResultsList";
 
 const navigator = createStackNavigator({
   Search: SearchScreen,
+  Results: ResultsList
 },  
 {
   initialRouteName: 'Search',
--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -1,12 +1,19 @@
-import React, {useState, useEffect} from 'react'
+import React, {useState} from 'react'
 import { View, Text, StyleSheet } from 'react-native'
 import SearchBar from '../components/SearchBar'
 import useResults from '../hooks.js/useResults'
+import ResultsList from '../components/ResultsList'
 
 const SearchScreen = () => {
     const [term, setTerm] = useState('')
     const [searchApi, results, errMessage] = useResults()
 
+    const filterResultsByPrice = (price) => {
+        return results.filter(result => {
+            return result.price === price
+        })
+
+    }
 
     return(
         <View>
@@ -17,6 +24,10 @@ const SearchScreen = () => {
             />
             { errMessage ? <Text>{errMessage}</Text> : null }
             <Text>We have found {results.length} results.</Text>
+            <ResultsList results={filterResultsByPrice('$')} title='Cost Effective' />
+            <ResultsList results={filterResultsByPrice('$$')} title='Bit Pricier'/>
+            <ResultsList results={filterResultsByPrice('$$$')}title='Most expensive'/>
+        
         </View>
     )
 }
